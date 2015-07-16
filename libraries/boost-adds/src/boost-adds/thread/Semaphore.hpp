83b1c68233a670cf70ba4b1154a572379561b246
Eloi DU BOIS <eloi.du.bois@gmail.com>
2015-07-17 01:13:31 +0200

    boost-adds: fixed multithreading issue with semaphore.

--- libraries/boost-adds/src/boost-adds/thread/Semaphore.hpp
+++ libraries/boost-adds/src/boost-adds/thread/Semaphore.hpp
@@ -19,6 +19,7 @@ public:
      */
     void post( const int n = 1 )
     {
+        std::lock_guard<std::mutex> lock( _mutexRing );
         _availableRings += n;
         if ( _availableRings > 0 )
         {
@@ -38,6 +39,7 @@ public:
      */
     inline bool take()
     {
+        std::lock_guard<std::mutex> lock( _mutexRing );
         if ( _availableRings > 0 )
         {
             return _availableRings-- > 0;
@@ -55,7 +57,8 @@ public:
     { _availableRings = 0; }
 
 private:
-    std::atomic_int _availableRings{0};             ///< Rings available
+    int _availableRings{0};                         ///< Rings available
+    std::mutex _mutexRing;                          ///< Mutex thread
     std::mutex _mutexSynchro;                       ///< Mutex thread
     std::condition_variable _synchroCondition;      ///< Synchronization condition
     std::unique_lock<std::mutex> _synchro;          ///< Lock for condition variable
