e322e044d75f23751352819ddfa681e34bac0d45
Eloi DU BOIS <eloi.du.bois@gmail.com>
2015-09-07 22:44:34 +0200

    cmake: minor fixes (icon, ...)

--- cmake/UseMvpPlayerBoost.cmake
+++ cmake/UseMvpPlayerBoost.cmake
@@ -3,6 +3,7 @@
 set(Boost_USE_STATIC_LIBS OFF)
 add_definitions( -DBOOST_MPL_CFG_NO_PREPROCESSED_HEADERS)
 add_definitions( -DBOOST_MPL_LIMIT_LIST_SIZE=30 )
+add_definitions( -DBOOST_ALL_DYN_LINK )
 find_package(Boost 1.58.0 
     COMPONENTS regex date_time chrono thread serialization system filesystem atomic log program_options timer QUIET)
 
